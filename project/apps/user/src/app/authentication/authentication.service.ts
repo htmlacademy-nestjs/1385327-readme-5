9aab6b92ddcd4d762ad6607f3a5d76b23fa9f21e
OksMor <sancho_2006_ru@mail.ru>
2023-12-10 18:51:08 +0300

    feat(user): add register verifyUser getUser

--- project/apps/user/src/app/authentication/authentication.service.ts
+++ project/apps/user/src/app/authentication/authentication.service.ts
@@ -1,9 +1,55 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
+
+import { AuthUserMessage } from './authentication.constant';
+import { CreateUserDto } from './dto/create-user.dto';
+import { LoginUserDto } from './dto/login-user.dto';
+
 import { BlogUserRepository } from '../blog-user/blog-user.repository';
+import { BlogUserEntity } from '../blog-user/blog-user.entity';
 
 @Injectable()
 export class AuthenticationService {
   constructor(
     private readonly blogUserRepository: BlogUserRepository
   ) {}
+
+  public async register(dto: CreateUserDto) {
+    const {email, name, avatarPath, password} = dto;
+
+    const blogUser = {
+      email, name, avatarPath, passwordHash: ''
+    };
+
+    const existUser = await this.blogUserRepository
+      .findByEmail(email);
+
+    if (existUser) {
+      throw new ConflictException(AuthUserMessage.Exists);
+    }
+
+    const userEntity = await new BlogUserEntity(blogUser)
+      .setPassword(password)
+
+    return this.blogUserRepository
+      .save(userEntity);
+  }
+
+  public async verifyUser(dto: LoginUserDto) {
+    const {email, password} = dto;
+    const existUser = await this.blogUserRepository.findByEmail(email);
+
+    if (!existUser) {
+      throw new NotFoundException(AuthUserMessage.NotFound);
+    }
+
+    if (!await existUser.comparePassword(password)) {
+      throw new UnauthorizedException(AuthUserMessage.PasswordWrong);
+    }
+
+    return existUser;
+  }
+
+  public async getUser(id: string) {
+    return this.blogUserRepository.findById(id);
+  }
 }
