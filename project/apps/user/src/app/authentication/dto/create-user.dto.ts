2d0d5a815c8552678e682fbbfb5de56f2151ca96
OksMor <sancho_2006_ru@mail.ru>
2023-12-10 20:01:36 +0300

    feat(user): add spec

--- project/apps/user/src/app/authentication/dto/create-user.dto.ts
+++ project/apps/user/src/app/authentication/dto/create-user.dto.ts
@@ -1,6 +1,27 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateUserDto {
+  @ApiProperty({
+    description: 'User unique address',
+    example: 'user@user.ru'
+  })
   public email: string;
+
+  @ApiProperty({
+    description: 'User name',
+    example: 'Keks',
+  })
   public name: string;
+
+  @ApiProperty({
+    description: 'User avatar path',
+    example: '/images/user.png'
+  })
   public avatarPath: string;
+
+  @ApiProperty({
+    description: 'User password',
+    example: '123456'
+  })
   public password: string;
 }
--- project/apps/user/src/app/authentication/rdo/user.rdo.ts
+++ project/apps/user/src/app/authentication/rdo/user.rdo.ts
@@ -1,16 +1,32 @@
 import { Expose } from 'class-transformer';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class UserRdo {
   @Expose()
+  @ApiProperty({
+    description: 'The uniq user ID',
+    example: '13'
+  })
   public id: string;
 
   @Expose()
+  @ApiProperty({
+    description: 'User email',
+    example: 'user@user.local'
+  })
   public email: string;
 
   @Expose()
+  @ApiProperty({
+    description: 'User name',
+    example: 'Keks'
+  })
   public name: string;
 
   @Expose()
+  @ApiProperty({
+    description: 'User avatar path',
+    example: '/images/user.png'
+  })
   public avatarPath: string;
-
 }
